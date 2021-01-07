afdb02588c4a88cc4742e5c24231ba05c9335249
Alex <alex@AlexdeMacBook-Pro.local>
2021-01-07 21:13:18 +0800

    edit

--- untitled.sh
+++ untitled.sh
@@ -9,13 +9,13 @@ cm="$GIT_COMMITTER_EMAIL"
  
 if [ "$GIT_COMMITTER_EMAIL" = "jackie_anxis@foxmail.com" ]
 then
-    cn="gozo1234"
-    cm="479815255@qq.com"
+    cn="yueziyao"
+    cm="351599628@qq.com"
 fi
 if [ "$GIT_AUTHOR_EMAIL" = "jackie_anxis@foxmail.com" ]
 then
-    an="gozo1234"
-    am="479815255@qq.com"
+    an="yueziyao"
+    am="351599628@qq.com"
 fi
  
 export GIT_AUTHOR_NAME="$an"
